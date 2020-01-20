5c37828f0ebea7980cbf9d9c4399636e37bd470b
Sebastian T F <sebastian.tharakan97@gmail.com>
2020-01-20 18:51:18 +0530

    add <PixelTable> in App.js with some styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import PixelTable  from "./components/pixelTableComponent.js";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <h2>The Scene Tables</h2>
+        <PixelTable rows="18" cols="18"></PixelTable>
+        <PixelTable rows="27" cols="48"></PixelTable>
+        <PixelTable rows="32" cols="32"></PixelTable>
       </header>
     </div>
   );
